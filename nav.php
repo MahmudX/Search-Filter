c1df5dbc171c8e42e4c6646f6f258257bc6f7b0c
mahmud-jerry <62949970+mahmud-jerry@users.noreply.github.com>
2020-07-18 14:55:34 +0600

    Added CSS and Javascript

---
+++ nav.php
@@ -0,0 +1,23 @@
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading" style="text-align: center;">Riko online</div>
+      <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Add user</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">User Information</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">New Payment</a>
+        <a href="#">Payment List</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Unpaid List</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">Advance Search</a>
+        <a href="#" class="list-group-item list-group-item-action bg-light">LogOut</a>
+      </div>
+    </div>
+    
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom " style="height:55px">
+
+</nav>
